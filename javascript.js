9808ff7e9bdd44c84c1d0479bb0c2211941fb537
Nyipoji Kani <nyip2019@gmail.com>
2025-09-21 22:27:14 +0100

    added findproduct

--- javascript.js
+++ javascript.js
@@ -9,6 +9,23 @@ function addStock(productName, id, price, quantity){
 inventory.push(stockDescription);
  
 }
+
+function findProduct(stockId) {
+   for (let i = 0; i < inventory.length; i++) {
+       if (inventory[i].id === stockId) {
+           return inventory[i];
+       }
+   }
+   return null;
+}
+ 
+function checkLowStock(product) {
+   if (product.quantity < 10) {
+       console.log("Warning:", product.productName, "is running low only", product.quantity, " items left");
+       return true;
+   }
+   return false;
+}
    
    function removeStock(productId){
    let product = findProduct(productId);
